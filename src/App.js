a3daa38574235effd8dc8fb923d1bb6772ddc859
Albert Wan <albertw@mastersystem.com>
2019-03-14 22:04:55 -0700

    initial work

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { withStyles } from '@material-ui/core';
 import './App.css';
 
+import grey from '@material-ui/core/colors/grey';
+
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    heroTitle: {
+        color: grey[500],
+    },
+};
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        const { classes } = this.props;
+        return (
+            <React.Fragment>
+                <div class='hero'>
+                    <div class='hero-inner'>
+                        <Typography className={classes.heroTitle} align='center' variant='h1'>
+                            Albert Wan
+                        </Typography>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
-export default App;
+export default withStyles(styles)(App);
